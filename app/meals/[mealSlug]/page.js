e1cfd9d22048d4ebe0243b027653f3aae1eb3047
ankur <ankur.tyagi.qls@gmail.com>
2026-02-13 16:53:20 +0530

    initial commit

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,4 @@
+export default function MealDetailsPage({ params }) {
+  return <h1>Meals Details {params.mealSlug}</h1>;
+}
+ 
---
+++ app/page.js
@@ -0,0 +1,15 @@
+import MainHeader from "@/components/main-header";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
+      <p><Link href="/meals">Meals</Link></p>
+      <p><Link href="/meals/share">Share Meal</Link></p>
+      <p><Link href="/community">Community</Link></p>
+    </main>
+  );
+}
